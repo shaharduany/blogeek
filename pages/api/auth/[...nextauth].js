249b84a5c3571acb8c795d0b56b072bec6b608d2
Shahar Duany <shahar.duany@gmail.com>
2022-04-22 14:54:37 +0300

    Made progress with authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,44 @@
 import NextAuth from "next-auth";
-import GoogleProvider from 'next-auth/providers/google';
-import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
-    secret: "thisisweirdiHopeItWorks",
-    session: {
-        strategy: "jwt",
-        maxAge: 60*80*24*3, //3 days
-        updateAge: 60*60&24,
-    },
-    providers: {
-        
-    },
+  secret: "thisisweirdiHopeItWorks",
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 80 * 24 * 3, //3 days
+    updateAge: (60 * 60) & 24,
+  },
+  providers: [
+    GoogleProvider({
+      clientId: "GOOGLE_CLIENT_ID",
+      clientSecret: "GOOGLE_CLIENT_SECRET",
+      authorizationUrl:
+        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
+    }),
+    CredentialsProvider({
+      name: "Email & password",
+      credentials: {
+        email: {
+          label: "email",
+          type: "email",
+          placeholder: "Your email",
+        },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "Your password",
+        },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const { email, password } = credentials;
+        if (email) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    }),
+  ],
 });
