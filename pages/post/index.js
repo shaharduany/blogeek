a8f16a6e8086801d0d11c9e1ebe51cdd7c349fbc
Shahar Duany <shahar.duany@gmail.com>
2022-04-28 18:24:48 +0300

    Made progress with /api/post/

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,20 +1,43 @@
 import axios from "axios";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import PostComp from "../../components/posts/Post";
+import { getRecentPosts } from "../../lib/db/db";
 
-function PostPage(props){
+function PostPage(props) {
+	let posts = props.posts;
+	const [page, setPage] = useState(1);
 
-    return (<Fragment>
-        <h1>POST PAGE</h1>
-        <h3>message > {props.message}</h3>
-    </Fragment>);
+	return (
+		<Fragment>
+			<h1>POST PAGE</h1>
+			{posts &&
+				posts.map((value, index) => (
+					<div key={index}>
+						<PostComp
+							id={value.id}
+							title={value.title}
+							content={value.content}
+							publisher={value.publusher}
+							date={value.date}
+							comments={value.comments}
+						/>
+					</div>
+				))}
+		</Fragment>
+	);
 }
 
-export async function getStaticProps(context){
-    return {
-        props: {
-            message: "hello"
-        },
-    };
+export async function getStaticProps(context) {
+	let posts = await getRecentPosts();
+	posts = JSON.parse(posts);
+
+    
+	return {
+		props: {
+			posts,
+		},
+		revalidate: 30,
+	};
 }
 
 export default PostPage;
